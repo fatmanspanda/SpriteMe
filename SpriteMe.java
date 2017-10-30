ed817adc9049eefe72e34f279f249198e0bd3fb7
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-10-30 16:15:51 -0400

    GUI
    Add some basic GUI stuff to start off
    SpringLayout is pretty nifty

--- SpriteMe.java
+++ SpriteMe.java
@@ -1,10 +1,25 @@
 package SpriteMe;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import SpriteManipulator.SpriteManipulator;
 
 public class SpriteMe {
+	private static final String VERSION = "v0.0.0";
 
 	public static final byte[][] VANILLAPALETTES = {
 			// Green mail
@@ -77,7 +92,83 @@ public class SpriteMe {
 			{ (byte) 192, (byte) 128, (byte) 240 }
 		};
 	
+	/*
+	 * Combo box constants
+	 */
+	private static final ColorPair[] SKINCOLORS = new ColorPair[] {
+			ColorPair.WHITE_SKIN,
+			ColorPair.PALE_SKIN,
+			ColorPair.HISPANIC_SKIN,
+			ColorPair.BROWN_SKIN,
+			ColorPair.BLACK_SKIN
+	};
+
 	public static void main(String[] args) {
+		//try to set LaF
+		try {
+			UIManager.setLookAndFeel("metal");
+		} catch (UnsupportedLookAndFeelException
+				| ClassNotFoundException
+				| InstantiationException
+				| IllegalAccessException e) {
+			// try to set System default
+			try {
+				UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			} catch (UnsupportedLookAndFeelException
+					| ClassNotFoundException
+					| InstantiationException
+					| IllegalAccessException e2) {
+					// do nothing
+			} //end System
+		} // end Metal
 		
+		// main window
+		final Dimension d = new Dimension(500,500);
+		final JFrame frame = new JFrame("Sprite Me " + VERSION);
+		final JPanel controls = new JPanel(new GridBagLayout());
+		GridBagConstraints w = new GridBagConstraints();
+		final JComboBox<ColorPair> skinPick = new JComboBox<ColorPair>(SKINCOLORS);
+		final JPanel image = new JPanel();
+		w.gridy = 0;
+		controls.add(skinPick, w);
+
+		
+		// format frame
+		final Container fullWrap = frame.getContentPane();
+		SpringLayout l = new SpringLayout();
+		frame.setLayout(l);
+		// add image
+		l.putConstraint(SpringLayout.WEST, image, 5,
+				SpringLayout.WEST, fullWrap);
+		l.putConstraint(SpringLayout.NORTH, image, 5,
+				SpringLayout.NORTH, fullWrap);
+		frame.add(image);
+
+		// add controls
+		l.putConstraint(SpringLayout.EAST, controls, -5,
+				SpringLayout.EAST, fullWrap);
+		l.putConstraint(SpringLayout.NORTH, controls, 5,
+				SpringLayout.NORTH, fullWrap);
+		frame.add(controls);
+		
+
+		// display frame
+		frame.setPreferredSize(d);
+		frame.setMinimumSize(d);
+		setAllSizes(frame, d);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLocation(150, 150);
+		frame.setVisible(true);
+	}
+	
+	/**
+	 * Set min max and preferred size for a component
+	 * @param c
+	 * @param d
+	 */
+	private static void setAllSizes(Component c, Dimension d) {
+		c.setPreferredSize(d);
+		c.setMaximumSize(d);
+		c.setMinimumSize(d);
 	}
 }
