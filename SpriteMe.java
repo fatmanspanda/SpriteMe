d33836262c030f1b97edeb3a985e6263fa4905d1
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-11-04 13:58:20 -0400

    Menu and Exports

--- SpriteMe.java
+++ SpriteMe.java
@@ -6,28 +6,43 @@ import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Image;
+import java.awt.TextArea;
 import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.KeyStroke;
 import javax.swing.SpringLayout;
 import javax.swing.SwingConstants;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.border.Border;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+
+import SpriteAnimator.GUIHelpers;
+import SpriteManipulator.SpriteManipulator;
 
 public class SpriteMe {
 	private static final String VERSION = "v0.0.0";
 	public static final int SPLOTCH_SIZE = 16;
 	public static final Dimension SPLOTCH_DIMENSION = new Dimension(SPLOTCH_SIZE, SPLOTCH_SIZE);
 	private static final Border rightPad = BorderFactory.createEmptyBorder(0,0,0,5);
+
 	/*
 	 * Combo box constants
 	 */
@@ -47,6 +62,12 @@ public class SpriteMe {
 		SpritePart.NOTHING, SpritePart.GLASSES,	
 	};
 
+	// file type constants
+	static final String[] SPRITE_ME_EXTS = { "sme" }; // SpriteMe files
+	static final String[] EXPORT_EXTS = { "spr" }; // sprite files
+	static final String[] ROM_EXTS = { "sfc" }; // ROM
+
+	// main
 	public static void main(String[] args) {
 		//try to set LaF
 		try {
@@ -65,7 +86,7 @@ public class SpriteMe {
 					// do nothing
 			} //end System
 		} // end Metal
-		
+
 		// main window
 		final Dimension d = new Dimension(1000,700);
 		final JFrame frame = new JFrame("Sprite Me " + VERSION);
@@ -84,7 +105,7 @@ public class SpriteMe {
 		controls.add(mailLbl,w);
 		w.gridx = 1;
 		controls.add(mailPick, w);
-		
+
 		// skin color
 		final JLabel skinLbl = new JLabel("Skin color", SwingConstants.RIGHT);
 		final JComboBox<ColorPair> skinPick = new JComboBox<ColorPair>(SKINCOLORS);
@@ -135,7 +156,7 @@ public class SpriteMe {
 		f.putConstraint(SpringLayout.EAST, pal, 0,
 				SpringLayout.EAST, framesWrap);
 		framesWrap.add(pal);
-		
+
 		// sprite appearance
 		IndexedSprite mySprite = new IndexedSprite(pal);
 		l.putConstraint(SpringLayout.NORTH, mySprite, 5,
@@ -148,7 +169,7 @@ public class SpriteMe {
 		ColorEditor colorEditor = new ColorEditor(pal);
 		SpritePartEditor indexMapEditor = new SpritePartEditor(pal);
 		pal.attachEditor(colorEditor);
-		
+
 		// wrapper frame
 		f.putConstraint(SpringLayout.NORTH, fullWrap, 2,
 				SpringLayout.NORTH, framesWrap);
@@ -157,7 +178,7 @@ public class SpriteMe {
 		f.putConstraint(SpringLayout.WEST, fullWrap, 2,
 				SpringLayout.WEST, framesWrap);
 		framesWrap.add(fullWrap);
-		
+
 		// color editor
 		f.putConstraint(SpringLayout.NORTH, colorEditor, 2,
 				SpringLayout.SOUTH, pal);
@@ -166,7 +187,7 @@ public class SpriteMe {
 		f.putConstraint(SpringLayout.EAST, colorEditor, -2,
 				SpringLayout.EAST, framesWrap);
 		framesWrap.add(colorEditor);
-		
+
 		// index map
 		l.putConstraint(SpringLayout.SOUTH, indexMapEditor, -2,
 				SpringLayout.SOUTH, fullWrap);
@@ -174,8 +195,101 @@ public class SpriteMe {
 				SpringLayout.EAST, fullWrap);
 		fullWrap.add(indexMapEditor);
 
-		// TODO : Credits
-		// bazly + fish for images
+		// menu
+		final JMenuBar menu = new JMenuBar();
+		frame.setJMenuBar(menu);
+
+		// file menu
+		final JMenu fileMenu = new JMenu("File");
+		menu.add(fileMenu);
+
+		// File quicksave
+		final JMenuItem saveSpr = new JMenuItem("Save");
+		saveSpr.setAccelerator(KeyStroke.getKeyStroke(
+			KeyEvent.VK_S, ActionEvent.CTRL_MASK));
+		fileMenu.add(saveSpr);
+
+		// File save
+		final JMenuItem saveSprTo = new JMenuItem("Save as");
+		saveSprTo.setAccelerator(KeyStroke.getKeyStroke(
+			KeyEvent.VK_S, ActionEvent.CTRL_MASK | ActionEvent.SHIFT_MASK));
+		fileMenu.add(saveSprTo);
+
+		// SPR quicksave
+		final JMenuItem expSpr = new JMenuItem("Export as SPR");
+		expSpr.setAccelerator(KeyStroke.getKeyStroke(
+			KeyEvent.VK_E, ActionEvent.CTRL_MASK));
+		fileMenu.add(expSpr);
+
+		// SPR save
+		final JMenuItem expSprTo = new JMenuItem("Export as SPR to...");
+		expSprTo.setAccelerator(KeyStroke.getKeyStroke(
+			KeyEvent.VK_E, ActionEvent.CTRL_MASK | ActionEvent.SHIFT_MASK));
+		fileMenu.add(expSprTo);
+
+		fileMenu.addSeparator();
+
+		// exit
+		final JMenuItem exit = new JMenuItem("Exit");
+		fileMenu.add(exit);
+		exit.addActionListener(arg0 -> System.exit(0));
+		// end file menu
+
+		// help menu
+		final JMenu helpMenu = new JMenu("Help");
+		menu.add(helpMenu);
+
+		// saved names used for quick saving/exporting
+		FakeString lastSavePath = new FakeString(null);
+		FakeString lastSpritePath = new FakeString(null);
+
+		// file explorer
+		final JFileChooser explorer = new JFileChooser() {
+			private static final long serialVersionUID = -7581065406880416887L;
+			public void approveSelection() {
+				File f = getSelectedFile();
+				if(f.exists() && getDialogType() == SAVE_DIALOG){
+					int result = JOptionPane.showConfirmDialog(
+							this,
+							"The file \"" + f.getName() + "\" already exists. Save anyway?",
+							"Existing file",
+							JOptionPane.YES_NO_CANCEL_OPTION);
+					switch(result){
+						case JOptionPane.YES_OPTION :
+							super.approveSelection();
+							return;
+						case JOptionPane.NO_OPTION :
+						case JOptionPane.CLOSED_OPTION :
+							return;
+						case JOptionPane.CANCEL_OPTION:
+							cancelSelection();
+							return;
+					}
+				}
+				super.approveSelection();
+			}
+		};
+
+		explorer.setCurrentDirectory(new File(".")); // quick way to set to current .jar loc
+
+		// set filters
+		FileNameExtensionFilter smeFilter =
+				new FileNameExtensionFilter("SpriteMe files", SPRITE_ME_EXTS);
+		FileNameExtensionFilter sprFilter =
+				new FileNameExtensionFilter("ALttP Sprite files", EXPORT_EXTS);
+		FileNameExtensionFilter romFilter =
+				new FileNameExtensionFilter("ROM files", ROM_EXTS);
+
+		explorer.setAcceptAllFileFilterUsed(false);
+
+		// Acknowledgements
+		final JMenuItem peeps = new JMenuItem("About");
+		helpMenu.add(peeps);
+		buildAbout();
+		peeps.addActionListener(arg0 -> aboutFrame.setVisible(true)); // do it here because short
+		// end help menu
+
+		// icon
 		ImageIcon ico = new ImageIcon(
 				SpriteMe.class.getResource("/SpriteMe/Images/Link thinking small.png")
 			);
@@ -194,14 +308,14 @@ public class SpriteMe {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setLocation(200, 100);
 		frame.setVisible(true);
-		
+
 		// repainting on all sprite changes
 		SpriteChangeListener repainter =
 				arg0 -> {
 					indexMapEditor.refreshPalette();
 					frame.repaint();
 				};
-		
+
 		indexMapEditor.addSpriteChangeListener(repainter);
 		pal.addSpriteChangeListener(repainter);
 		mySprite.addSpriteChangeListener(repainter);
@@ -228,6 +342,101 @@ public class SpriteMe {
 					indexMapEditor.editNewPart(picked);
 				}
 			});
+
+		// Action listeners for menu
+		// save spr
+		saveSpr.addActionListener(
+				arg0 -> {
+					if (lastSavePath.isSet()) {
+/*						try {
+							
+						} catch (IOException e) {
+							JOptionPane.showMessageDialog(frame,
+									"Error saving custom selections",
+									"WOW",
+									JOptionPane.WARNING_MESSAGE);
+							e.printStackTrace();
+						}*/
+					} else { // if nowhere to export, click "export to" to prompt a file selection
+						saveSprTo.doClick();
+					}
+				});
+
+		saveSprTo.addActionListener(
+				arg0 -> {
+					removeFilters(explorer);
+					explorer.setFileFilter(smeFilter);
+					int option = explorer.showSaveDialog(frame);
+
+					if (option == JFileChooser.CANCEL_OPTION) {
+						return;
+					}
+
+					String n = "";
+					try {
+						n = explorer.getSelectedFile().getPath();
+					} catch (NullPointerException e) {
+						// do nothing
+					} finally {
+						if (!SpriteManipulator.testFileType(n,SPRITE_ME_EXTS)) {
+							if(!n.contains(".") && n.length() > 0) { // no filetype, append spr
+								n = n + ".sme";
+							} else { // otherwise break out
+								return;
+							}
+						}
+						// if we have a file name, set it then click export
+						lastSavePath.changeString(n);
+						saveSpr.doClick();
+					}
+				});
+		// sprite exporting
+		expSpr.addActionListener(
+				arg0 -> {
+					if (lastSpritePath.isSet()) {
+						byte[] data = mySprite.makeSprite();
+						try {
+							SpriteManipulator.writeSPR(data, lastSpritePath.toString());
+						} catch (IOException e) {
+							JOptionPane.showMessageDialog(frame,
+									"Error exporting to a SPR file.",
+									"WOW",
+									JOptionPane.WARNING_MESSAGE);
+							e.printStackTrace();
+						}
+					} else { // if nowhere to export, click "export to" to prompt a file selection
+						expSprTo.doClick();
+					}
+				});
+
+		expSprTo.addActionListener(
+				arg0 -> {
+					removeFilters(explorer);
+					explorer.setFileFilter(sprFilter);
+					int option = explorer.showSaveDialog(frame);
+
+					if (option == JFileChooser.CANCEL_OPTION) {
+						return;
+					}
+
+					String n = "";
+					try {
+						n = explorer.getSelectedFile().getPath();
+					} catch (NullPointerException e) {
+						// do nothing
+					} finally {
+						if (!SpriteManipulator.testFileType(n,EXPORT_EXTS)) {
+							if(!n.contains(".") && n.length() > 0) { // no filetype, append spr
+								n = n + ".spr";
+							} else { // otherwise break out
+								return;
+							}
+						}
+						// if we have a file name, set it then click export
+						lastSpritePath.changeString(n);
+						expSpr.doClick();
+					}
+				});
 	}
 
 	/**
@@ -239,5 +448,52 @@ public class SpriteMe {
 		c.setPreferredSize(d);
 		c.setMaximumSize(d);
 		c.setMinimumSize(d);
+	}
+
+	/**
+	 * Removes all filters from an explorer.
+	 * @param ex
+	 */
+	private static void removeFilters(JFileChooser ex) {
+		FileFilter[] exlist = ex.getChoosableFileFilters();
+		for (FileFilter r : exlist) {
+			ex.removeChoosableFileFilter(r);
+		}
+	}
+
+	// about frame built here
+	static final JFrame aboutFrame = new JFrame("Acknowledgements");
+	private static void buildAbout() {
+		final TextArea peepsList = new TextArea("", 0,0,TextArea.SCROLLBARS_VERTICAL_ONLY);
+		peepsList.setEditable(false);
+		peepsList.append("Written by fatmanspanda"); // hey, that's me
+
+		peepsList.append("\n\nArt:\n");
+		peepsList.append(GUIHelpers.join(new String[]{
+				"iBazly",
+				"Fish"
+				}, ", "));
+
+		aboutFrame.add(peepsList);
+
+		aboutFrame.setSize(300,300);
+		aboutFrame.setLocation(150,150);
+		aboutFrame.setResizable(false);
+	}
+	
+	private static class FakeString {
+		private String s;
+		public FakeString(String s) {
+			this.s = s;
+		}
+		public void changeString(String s) {
+			this.s = s;
+		}
+		public String toString() {
+			return this.s;
+		}
+		public boolean isSet() {
+			return this.s != null;
+		}
 	}
 }
