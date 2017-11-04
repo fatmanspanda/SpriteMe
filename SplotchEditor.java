88b9bf888abf154e94162094c876995751188f17
fatmanspanda <32842036+fatmanspanda@users.noreply.github.com>
2017-11-04 10:03:48 -0400

    Merge classes
    Small class only used here for drawing
    Convention says it's okay to inner class

--- SplotchEditor.java
+++ SplotchEditor.java
@@ -1,7 +1,10 @@
 package SpriteMe;
 
+import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
@@ -436,4 +439,38 @@ public class SplotchEditor extends Container {
 			}
 		};
 	}
+
+	// preview class for drawing squares
+	private static class ColorPreview extends Component {
+		private static final long serialVersionUID = -6583423770149289548L;
+		public static final int SIZE = 40;
+		static final Dimension D = new Dimension(SIZE,SIZE);
+
+		// locals
+		private Color c = new Color(0,0,0);
+		public ColorPreview() {
+			this.setSize(D);
+			this.setPreferredSize(D);
+			this.setMinimumSize(D);
+			this.setMaximumSize(D);
+		}
+
+		/**
+		 * Sets a new color with RGB values.
+		 * @param rgb
+		 */
+		public void setColor(int[] rgb) {
+			c = new Color(rgb[0], rgb[1], rgb[2]);
+		}
+
+		/**
+		 * 
+		 */
+		public void paint(Graphics g) {
+			g.setColor(c);
+			g.fillRect(0, 0, SIZE, SIZE);
+			g.setColor(Color.BLACK);
+			g.drawRect(0, 0, SIZE-1, SIZE-1);
+		}
+	} // end ColorPreview
 }
